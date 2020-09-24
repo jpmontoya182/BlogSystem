410a381a6a2a7991b5664b5e947b0c3643292582
Juan P. Montoya <Juan.Montoya@viamericas.com>
2020-09-24 18:37:09 -0500

    updated structure folder

--- Documentation/BlogBD.sql
+++ Documentation/BlogBD.sql
@@ -1,6 +1,7 @@
+CREATE DATABASE [BlogBD]
+GO
 USE [BlogBD]
 GO
-/****** Object:  Table [dbo].[Comments]    Script Date: 9/22/2020 11:15:15 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -16,7 +17,7 @@ CREATE TABLE [dbo].[Comments](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Posts]    Script Date: 9/22/2020 11:15:15 PM ******/
+/****** Object:  Table [dbo].[Posts]    Script Date: 9/24/2020 6:32:58 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -27,14 +28,14 @@ CREATE TABLE [dbo].[Posts](
 	[PostContent] [varchar](50) NOT NULL,
 	[CreateDate] [datetime] NOT NULL,
 	[State] [bit] NOT NULL,
-	[UserId] [int] NOT NULL,
+	[UserId] [int] NULL,
  CONSTRAINT [PK_PostBlog] PRIMARY KEY CLUSTERED 
 (
 	[PostId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Profiles]    Script Date: 9/22/2020 11:15:15 PM ******/
+/****** Object:  Table [dbo].[Profiles]    Script Date: 9/24/2020 6:32:58 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -50,7 +51,7 @@ CREATE TABLE [dbo].[Profiles](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Users]    Script Date: 9/22/2020 11:15:15 PM ******/
+/****** Object:  Table [dbo].[Users]    Script Date: 9/24/2020 6:32:58 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -67,20 +68,6 @@ CREATE TABLE [dbo].[Users](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-SET IDENTITY_INSERT [dbo].[Comments] ON 
-GO
-INSERT [dbo].[Comments] ([CommentId], [ContentComment], [CreateDate], [PostId]) VALUES (1, N'Muy bueno gracias', CAST(N'2020-09-22T23:08:29.230' AS DateTime), 7)
-GO
-INSERT [dbo].[Comments] ([CommentId], [ContentComment], [CreateDate], [PostId]) VALUES (2, N'Gracias lo estaba buscando', CAST(N'2020-09-22T23:56:29.230' AS DateTime), 7)
-GO
-SET IDENTITY_INSERT [dbo].[Comments] OFF
-GO
-SET IDENTITY_INSERT [dbo].[Posts] ON 
-GO
-INSERT [dbo].[Posts] ([PostId], [Title], [PostContent], [CreateDate], [State], [UserId]) VALUES (7, N'OOP', N'This is a little ´post related to programming', CAST(N'2020-09-22T17:07:06.303' AS DateTime), 1, 2)
-GO
-SET IDENTITY_INSERT [dbo].[Posts] OFF
-GO
 SET IDENTITY_INSERT [dbo].[Profiles] ON 
 GO
 INSERT [dbo].[Profiles] ([ProfileId], [Name], [Description], [State]) VALUES (1, N'admin', N'Administrator', 1)
@@ -88,14 +75,6 @@ GO
 INSERT [dbo].[Profiles] ([ProfileId], [Name], [Description], [State]) VALUES (2, N'user', N'Usuario', 1)
 GO
 SET IDENTITY_INSERT [dbo].[Profiles] OFF
-GO
-SET IDENTITY_INSERT [dbo].[Users] ON 
-GO
-INSERT [dbo].[Users] ([UserId], [Username], [Password], [Email], [ProfileId]) VALUES (1, N'root', N'root', N'root@myblog.com', 1)
-GO
-INSERT [dbo].[Users] ([UserId], [Username], [Password], [Email], [ProfileId]) VALUES (2, N'pepito', N'pepito100', N'pepito@hotmail.com', 2)
-GO
-SET IDENTITY_INSERT [dbo].[Users] OFF
 GO
 ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_CommentBlog_PostBlog] FOREIGN KEY([PostId])
 REFERENCES [dbo].[Posts] ([PostId])
